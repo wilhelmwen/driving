1668284cd943cd66e3ce0e775790afc72e9143eb
william wen <wilhelm6939@gmail.com>
2019-03-06 22:26:12 +0800

    add US

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
-country = input('請問你是哪國人')
+country = input('請問你是哪國人: ')
 age = input('你輸入年齡: ')
 age = int(age)
 if country == '台灣':
 	if age >= 18:
+		print('你可以考駕照')
+	else:
+		print('你還不能考駕照')
+elif country == '美國':
+	if age >= 16:
 		print('你可以考駕照')
 	else:
 		print('你還不能考駕照')
